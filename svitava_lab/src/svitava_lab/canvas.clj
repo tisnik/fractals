a51cbdf2fc6af0b2d2e1a01f29d3552ba916a870
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-19 09:46:17 +0200

    Basic canvas implementation

---
+++ svitava_lab/src/svitava_lab/canvas.clj
@@ -0,0 +1,62 @@
+(ns svitava-lab.canvas)
+
+(defn create
+  "Create new canvas."
+  [title width height]
+  (let [frame    (new javax.swing.JFrame title)
+        panel    (.getContentPane frame)
+        size     (new java.awt.Dimension width height)]
+    (.setPreferredSize panel size)
+    (.setBackground panel java.awt.Color/white)
+    (.pack frame)
+    {:frame    frame
+     :panel    panel
+     :graphics (.getGraphics panel)}))
+
+(defn open
+  "Open new canvas on screen."
+  [title width height]
+  (let [canvas (create title width height)]
+    (.setVisible (:frame canvas) true)
+    canvas))
+
+(defn clear!
+  "Clear the canvas."
+  [canvas]
+  (let [panel    (:panel canvas)
+        graphics (:graphics canvas)]
+      (.setBackground panel (.getBackground panel))
+      (.clearRect graphics 0 0 (.getWidth panel) (.getHeight panel))))
+
+(defn close!
+  "Close the canvas."
+  [canvas]
+  (let [frame (:frame canvas)]
+    (doto frame
+      (.setVisible false)
+      (.dispose))))
+
+(defn set-color!
+  "Set drawing color."
+  [canvas color]
+  (let [graphics (:graphics canvas)]
+    (.setColor graphics color)))
+
+(defn draw!
+  "Draw a line."
+  [canvas x1 y1 x2 y2]
+  (let [graphics (:graphics canvas)]
+    (.drawLine graphics x1 y1 x2 y2)))
+
+(comment
+  ;; example of use
+
+(let [c (open "foo" 640 480)]
+  (clear! c)
+  (java.lang.Thread/sleep 1000)
+  (draw! c 0 0 640 480)
+  (java.lang.Thread/sleep 1000)
+  (close! c)
+  (java.lang.Thread/sleep 1000))
+
+)
