c79da01c9153dd9a7dfda86fb6a9bc53a34185fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-22 10:25:20 +0200

    BitBlt operation

--- svitava_lab/src/svitava_lab/canvas.clj
+++ svitava_lab/src/svitava_lab/canvas.clj
@@ -43,13 +43,36 @@
     (.setColor graphics color)))
 
 (defn draw!
-  "Draw a line."
+  "Draw a line onto canvas."
   [canvas x1 y1 x2 y2]
   (let [graphics (:graphics canvas)]
     (.drawLine graphics x1 y1 x2 y2)))
 
+(defn bitblt!
+  "Draw a bitmap onto canvas."
+  [canvas bitmap x y]
+  (let [graphics (:graphics canvas)]
+    (.drawImage graphics bitmap x y nil)))
+
 (comment
   ;; example of use
+
+(require '[svitava-lab.canvas :as canvas])
+(require '[svitava-lab.bitmap :as bitmap])
+
+(let [c (canvas/open "foo" 640 480)
+      b (bitmap/create 640 480)]
+  (bitmap/putpixel b 100 100 0x00 0x00 0xff)
+  (bitmap/putpixel b 101 100 0x00 0x00 0xff)
+  (bitmap/putpixel b 101 101 0x00 0x00 0xff)
+  (bitmap/putpixel b 100 101 0x00 0x00 0xff)
+  (canvas/clear! c)
+  (java.lang.Thread/sleep 1000)
+  (canvas/draw! c 0 0 640 480)
+  (canvas/bitblt! c b 0 0)
+  (java.lang.Thread/sleep 1000)
+  (canvas/close! c)
+  (java.lang.Thread/sleep 1000))
 
 (let [c (open "foo" 640 480)]
   (clear! c)
