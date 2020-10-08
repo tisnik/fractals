a0d476891cbcc1f2e26c36ec49a8b3ed2f85902a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-08 11:08:48 +0200

    Mandelbrot z^2-z+c variant

---
+++ complex/mandelbrot_z^2-z.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from PIL import Image
+import palette_blues
+
+# textura by mela byt ctvercova a jeji sirka i vyska by mela byt
+# mocninou cisla 2
+IMAGE_WIDTH = 256
+IMAGE_HEIGHT = 256
+
+
+def mandelbrot(cx, cy, maxiter):
+    c = complex(cx, cy)
+    z = 0
+    for i in range(0, maxiter):
+        if abs(z) > 2:
+            return i
+        z = z * z - z + c
+    return 0
+
+
+def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, maxiter=1000):
+    width, height = image.size       # rozmery obrazku
+    stepx = (xmax - xmin)/width
+    stepy = (ymax - ymin)/height
+
+    y1 = ymin
+    for y in range(0, height):
+        x1 = xmin
+        for x in range(0, width):
+            i = mandelbrot(x1, y1, maxiter)
+            i = 3*i % 256
+            color = (palette[i][0], palette[i][1], palette[i][2])
+            image.putpixel((x, y), color)
+            x1 += stepx
+        y1 += stepy
+
+
+image = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+recalc_fractal(image, palette_blues.palette, -2.0, -1.5, 1.0, 1.5, 1000)
+image.save("mandelbrot_z^2-z.png")
