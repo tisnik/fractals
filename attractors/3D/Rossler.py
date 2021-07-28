57b11eee3f57ec9b38f87e6ebbd969e77f148182
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-28 11:33:40 +0200

    Added docstrings

--- attractors/3D/Rossler.py
+++ attractors/3D/Rossler.py
@@ -38,6 +38,7 @@ import numpy as np
 
 # funkce pro výpočet dalšího bodu Rosslerova atraktoru
 def rossler(x, y, z, a=0.2, b=0.2, c=5.7):
+    """Výpočet dalšího bodu Rosslerova atraktoru."""
     x_dot = -y - z
     y_dot = x + a*y
     z_dot = b + z*(x-c)
@@ -123,4 +124,3 @@ plt.tight_layout()
 
 plt.tight_layout()
 plt.show()
-
