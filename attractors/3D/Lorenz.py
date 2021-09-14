77fa1b8c23d539feb3c5d1a322294f71e2bf30c7
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-09-14 08:11:43 +0200

    Added comments

--- attractors/3D/Lorenz.py
+++ attractors/3D/Lorenz.py
@@ -29,6 +29,7 @@ import numpy as np
 
 # funkce pro výpočet dalšího bodu Lorenzova atraktoru
 def lorenz(x, y, z, s=10, r=28, b=2.667):
+    """Výpočet dalšího bodu Lorenzova atraktoru."""
     x_dot = s*(y - x)
     y_dot = r*x - y - x*z
     z_dot = x*y - b*z
