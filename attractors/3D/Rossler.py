8b7e5760eff69819e53b3619ccd790f95de94431
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-02 10:29:38 +0100

    Reformatted by Black

--- attractors/3D/Rossler.py
+++ attractors/3D/Rossler.py
@@ -40,8 +40,8 @@ import numpy as np
 def rossler(x, y, z, a=0.2, b=0.2, c=5.7):
     """Výpočet dalšího bodu Rosslerova atraktoru."""
     x_dot = -y - z
-    y_dot = x + a*y
-    z_dot = b + z*(x-c)
+    y_dot = x + a * y
+    z_dot = b + z * (x - c)
     return x_dot, y_dot, z_dot
 
 
@@ -67,14 +67,14 @@ z = np.zeros((n,))
 x[0], y[0], z[0] = (0.1, 0.1, 6)
 
 # vlastní výpočet atraktoru
-for i in range(n-1):
+for i in range(n - 1):
     x_dot, y_dot, z_dot = rossler(x[i], y[i], z[i], 0.3, 0.2, 5.7)
-    x[i+1] = x[i] + x_dot * dt
-    y[i+1] = y[i] + y_dot * dt
-    z[i+1] = z[i] + z_dot * dt
+    x[i + 1] = x[i] + x_dot * dt
+    y[i + 1] = y[i] + y_dot * dt
+    z[i + 1] = z[i] + z_dot * dt
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.gca(projection="3d")
 
 # vykreslení grafu
 ax.plot(x[50000:], y[50000:], z[50000:])
@@ -90,32 +90,32 @@ plt.show()
 ch_3d = np.stack((x, y, z))
 lim_xyz = [(np.min(ch_3d[ii]), np.max(ch_3d[ii])) for ii in range(3)]
 
-fig2 = plt.figure('3D Coordinates')
+fig2 = plt.figure("3D Coordinates")
 plt.subplot(2, 2, 1)
 plt.plot(y, x, linewidth=0.75)
 plt.grid()
-plt.xlabel('X')
-plt.ylabel('Y')
+plt.xlabel("X")
+plt.ylabel("Y")
 plt.xlim(lim_xyz[1])
 plt.ylim(lim_xyz[0])
 
 plt.subplot(2, 2, 2)
 plt.plot(y, z, linewidth=0.75)
 plt.grid()
-plt.xlabel('Z')
-plt.ylabel('Y')
+plt.xlabel("Z")
+plt.ylabel("Y")
 plt.xlim(lim_xyz[1])
 plt.ylim(lim_xyz[2])
 
 plt.subplot(2, 2, 3)
 plt.plot(z, x, linewidth=0.75)
 plt.grid()
-plt.xlabel('X')
-plt.ylabel('Z')
+plt.xlabel("X")
+plt.ylabel("Z")
 plt.xlim(lim_xyz[2])
 plt.ylim(lim_xyz[0])
 
-ax = fig2.add_subplot(2, 2, 4, projection='3d')
+ax = fig2.add_subplot(2, 2, 4, projection="3d")
 ax.plot(x, y, z, linewidth=0.7)
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
