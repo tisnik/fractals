d2f89c0078aec35787cf0317887c84f4dab9553a
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-02 10:47:54 +0100

    Formatted by Black

--- complex/blend_add.py
+++ complex/blend_add.py
@@ -22,16 +22,16 @@ def mandelbrot(cx, cy, maxiter):
 
 def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, maxiter=1000):
     """Přepočet celého fraktálu."""
-    width, height = image.size       # rozměry obrázku
-    stepx = (xmax - xmin)/width
-    stepy = (ymax - ymin)/height
+    width, height = image.size  # rozměry obrázku
+    stepx = (xmax - xmin) / width
+    stepy = (ymax - ymin) / height
 
     y1 = ymin
     for y in range(0, height):
         x1 = xmin
         for x in range(0, width):
             i = mandelbrot(x1, y1, maxiter)
-            i = 3*i % 256
+            i = 3 * i % 256
             color = (palette[i][0], palette[i][1], palette[i][2])
             image.putpixel((x, y), color)
             x1 += stepx
@@ -43,9 +43,13 @@ def addImages(image1, image2):
     image = Image.new("RGB", (width, height))
     for y in range(0, height):
         for x in range(0, width):
-            color1 = image1.getpixel((x,y))
-            color2 = image2.getpixel((x,y))
-            color = ((color1[0]+color2[0])//2, (color1[1]+color2[1])//2, (color1[2]+color2[2])//2)
+            color1 = image1.getpixel((x, y))
+            color2 = image2.getpixel((x, y))
+            color = (
+                (color1[0] + color2[0]) // 2,
+                (color1[1] + color2[1]) // 2,
+                (color1[2] + color2[2]) // 2,
+            )
             image.putpixel((x, y), color)
     return image
 
@@ -53,13 +57,25 @@ def addImages(image1, image2):
 image1 = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
 image2 = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
 
-recalc_fractal(image1, palette_blues.palette,
-               -0.769824999999999998320, -0.109270000000000000000,
-               -0.766247499999999998426, -0.106570000000000000000, 1000)
+recalc_fractal(
+    image1,
+    palette_blues.palette,
+    -0.769824999999999998320,
+    -0.109270000000000000000,
+    -0.766247499999999998426,
+    -0.106570000000000000000,
+    1000,
+)
 
-recalc_fractal(image2, palette_greens.palette,
-               -0.207190825000000012496, 0.676656624999999999983,
-               -0.206107925000000012496, 0.677468799999999999983, 1000)
+recalc_fractal(
+    image2,
+    palette_greens.palette,
+    -0.207190825000000012496,
+    0.676656624999999999983,
+    -0.206107925000000012496,
+    0.677468799999999999983,
+    1000,
+)
 
 image3 = addImages(image1, image2)
 image3.save("blend_add.png")
