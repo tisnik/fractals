d6bce0e1a43b0f2426ac79afcd1eb572ebf32bea
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-02 10:47:54 +0100

    Formatted by Black

--- complex/alpha_composite.py
+++ complex/alpha_composite.py
@@ -22,9 +22,9 @@ def mandelbrot(cx, cy, maxiter):
 
 def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, maxiter=1000):
     """Přepočet celého fraktálu."""
-    width, height = image.size       # rozměry obrázku
-    stepx = (xmax - xmin)/width
-    stepy = (ymax - ymin)/height
+    width, height = image.size  # rozměry obrázku
+    stepx = (xmax - xmin) / width
+    stepy = (ymax - ymin) / height
 
     y1 = ymin
     for y in range(0, height):
@@ -32,7 +32,7 @@ def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, maxiter=1000):
         for x in range(0, width):
             i = mandelbrot(x1, y1, maxiter)
             alpha = i
-            i = 3*i % 256
+            i = 3 * i % 256
             color = (palette[i][0], palette[i][1], palette[i][2], alpha)
             image.putpixel((x, y), color)
             x1 += stepx
@@ -42,13 +42,25 @@ def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, maxiter=1000):
 image1 = Image.new("RGBA", (IMAGE_WIDTH, IMAGE_HEIGHT))
 image2 = Image.new("RGBA", (IMAGE_WIDTH, IMAGE_HEIGHT))
 
-recalc_fractal(image1, palette_mandmap.palette,
-               -0.769824999999999998320, -0.109270000000000000000,
-               -0.766247499999999998426, -0.106570000000000000000, 1000)
+recalc_fractal(
+    image1,
+    palette_mandmap.palette,
+    -0.769824999999999998320,
+    -0.109270000000000000000,
+    -0.766247499999999998426,
+    -0.106570000000000000000,
+    1000,
+)
 
-recalc_fractal(image2, palette_mandmap.palette,
-               -0.207190825000000012496, 0.676656624999999999983,
-               -0.206107925000000012496, 0.677468799999999999983, 1000)
+recalc_fractal(
+    image2,
+    palette_mandmap.palette,
+    -0.207190825000000012496,
+    0.676656624999999999983,
+    -0.206107925000000012496,
+    0.677468799999999999983,
+    1000,
+)
 
 image3 = Image.alpha_composite(image1, image2)
 image3.save("alpha_composite.png")
