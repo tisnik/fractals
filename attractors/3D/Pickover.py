a0fc21e074948392b2b7c7023427873d46c18fae
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-02 10:29:38 +0100

    Reformatted by Black

--- attractors/3D/Pickover.py
+++ attractors/3D/Pickover.py
@@ -59,17 +59,17 @@ z = np.zeros((n,))
 x[0], y[0], z[0] = (0.0, 0.0, 0.0)
 
 # vlastní výpočet atraktoru
-for i in range(n-1):
+for i in range(n - 1):
     x_dot, y_dot, z_dot = pickover(x[i], y[i], z[i], 2.24, 0.43, -0.65, -2.43, 0.8)
-    x[i+1] = x_dot
-    y[i+1] = y_dot
-    z[i+1] = z_dot
+    x[i + 1] = x_dot
+    y[i + 1] = y_dot
+    z[i + 1] = z_dot
 
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.gca(projection="3d")
 
 # vykreslení grafu
-ax.plot(x, y, z, 'o', markersize=0.1)
+ax.plot(x, y, z, "o", markersize=0.1)
 
 # zobrazení grafu
 plt.tight_layout()
@@ -78,33 +78,33 @@ plt.tight_layout()
 ch_3d = np.stack((x, y, z))
 lim_xyz = [(np.min(ch_3d[ii]), np.max(ch_3d[ii])) for ii in range(3)]
 
-fig2 = plt.figure('3D Coordinates')
+fig2 = plt.figure("3D Coordinates")
 plt.subplot(2, 2, 1)
-plt.plot(y, x, 'o', linewidth=0.75, markersize=0.1)
+plt.plot(y, x, "o", linewidth=0.75, markersize=0.1)
 plt.grid()
-plt.xlabel('X')
-plt.ylabel('Y')
+plt.xlabel("X")
+plt.ylabel("Y")
 plt.xlim(lim_xyz[1])
 plt.ylim(lim_xyz[0])
 
 plt.subplot(2, 2, 2)
-plt.plot(y, z, 'o', linewidth=0.75, markersize=0.1)
+plt.plot(y, z, "o", linewidth=0.75, markersize=0.1)
 plt.grid()
-plt.xlabel('Z')
-plt.ylabel('Y')
+plt.xlabel("Z")
+plt.ylabel("Y")
 plt.xlim(lim_xyz[1])
 plt.ylim(lim_xyz[2])
 
 plt.subplot(2, 2, 3)
-plt.plot(z, x, 'o', linewidth=0.75, markersize=0.1)
+plt.plot(z, x, "o", linewidth=0.75, markersize=0.1)
 plt.grid()
-plt.xlabel('X')
-plt.ylabel('Z')
+plt.xlabel("X")
+plt.ylabel("Z")
 plt.xlim(lim_xyz[2])
 plt.ylim(lim_xyz[0])
 
-ax = fig2.add_subplot(2, 2, 4, projection='3d')
-ax.plot(x, y, z, 'o', linewidth=0.7, markersize=0.1)
+ax = fig2.add_subplot(2, 2, 4, projection="3d")
+ax.plot(x, y, z, "o", linewidth=0.7, markersize=0.1)
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Z")
