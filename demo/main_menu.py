60e98617443ac66b5f0ee7775743299072103874
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-11-16 08:33:30 +0100

    Main menu entries

---
+++ demo/main_menu.py
@@ -0,0 +1,29 @@
+# vim: set fileencoding=utf-8
+
+#
+#  (C) Copyright 2023  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+"""All entries from the main menu."""
+
+from enum import Enum
+
+
+class MainMenu(Enum):
+    """All entries from the main menu."""
+
+    PROCEDURAL_TEXTURES = 0
+    COMPLEX_FRACTALS = 1
+    STRANGE_ATTRACTORS = 2
+    IFS = 3
+    DYNAMIC_SYSTEMS = 4
+    ABOUT = 5
+    QUIT = 6
