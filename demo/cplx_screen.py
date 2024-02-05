e7d406629f2bcb03f2b797539b36bc1197589ea4
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-05 08:46:59 +0100

    Fixed fractals in complex plane menu

--- demo/cplx_screen.py
+++ demo/cplx_screen.py
@@ -1,7 +1,7 @@
 # vim: set fileencoding=utf-8
 
 #
-#  (C) Copyright 2023  Pavel Tisnovsky
+#  (C) Copyright 2023, 2024  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -33,7 +33,7 @@ class CplxScreen(AbstractMenuScreen):
         self._menu = (
             self.renderMenuItem("Classic Mandelbrot fractal"),
             self.renderMenuItem("Classic Julia fractal"),
-            self.renderMenuItem("Mandelbrot variant: z²"),
+            self.renderMenuItem("Mandelbrot variant: z³"),
             self.renderMenuItem("Mandelbrot variant: z⁴"),
             self.renderMenuItem("Mandelbrot variant: z²-z"),
             self.renderMenuItem("Mandelbrot variant: z³-z"),
