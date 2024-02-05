a02bb1b369356d34fc7f39397a0820333aabe5dc
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-05 08:46:36 +0100

    Updated menu items

--- demo/cplx_menu.py
+++ demo/cplx_menu.py
@@ -1,7 +1,7 @@
 # vim: set fileencoding=utf-8
 
 #
-#  (C) Copyright 2023  Pavel Tisnovsky
+#  (C) Copyright 2023, 2024  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -20,4 +20,11 @@ from enum import Enum
 class CplxMenu(Enum):
     """All entries from the complex fractals menu."""
 
-    QUIT = 4
+    MANDELBROT = 1
+    JULIA = 2
+    MANDELBROT_Z3 = 3
+    MANDELBROT_Z4 = 4
+    MANDELBROT_Z2_Z = 4
+    MANDELBROT_Z3_Z = 5
+
+    QUIT = 7
