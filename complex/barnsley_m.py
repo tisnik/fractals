bb2ee4be73dd4dee12c60c8470e966c429194bbf
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-05 08:47:29 +0100

    Barnsley Mandelbrot fractal

---
+++ complex/barnsley_m.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+"""Renderer of the classic Barnsley fractal."""
+
+from PIL import Image
+
+# textura by mela byt ctvercova a jeji sirka i vyska by mela byt
+# mocninou cisla 2
+IMAGE_WIDTH = 512
+IMAGE_HEIGHT = 512
+
+
+def barnsley_m(cx, cy, maxiter):
+    """Calculate number of iterations for given complex number to escape from set."""
+    c = complex(cx, cy)
+    z = 0
+    for i in range(0, maxiter):
+        if abs(z) > 2:
+            return i
+        if z.real >= 0:
+            z = (z - 1) * c
+        else:
+            z = (z + 1) * c
+
+    return 0
+
+
+def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, maxiter=1000):
+    """Recalculate the whole fractal and render the set into given image."""
+    width, height = image.size  # rozmery obrazku
+    stepx = (xmax - xmin) / width
+    stepy = (ymax - ymin) / height
+
+    y1 = ymin
+    for y in range(0, height):
+        x1 = xmin
+        for x in range(0, width):
+            i = barnsley_m(x1, y1, maxiter)
+            i = 3 * i % 256
+            color = (palette[i][0], palette[i][1], palette[i][2])
+            image.putpixel((x, y), color)
+            x1 += stepx
+        y1 += stepy
+
+
+def main():
+    import palette_blues
+
+    image = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+    recalc_fractal(image, palette_blues.palette, -2.0, -2.0, 2.0, 2.0, 500)
+    image.save("barnsley_m.png")
+
+
+if __name__ == "__main__":
+    main()
