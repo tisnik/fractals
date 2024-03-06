29c372e5ab22b8a8957d9f02f369ce42a117721c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-06 17:16:55 +0100

    Test RGB image rendering

--- svitava/svitava.c
+++ svitava/svitava.c
@@ -18,3 +18,18 @@ testlib.c build as executable:
 */
 
 
+void render_test_rgb_image(unsigned int width, unsigned int height,
+                           unsigned char *pixels, unsigned char green) {
+    unsigned int i, j;
+    unsigned char *p = pixels;
+
+    for (j = 0; j < height; j++) {
+        for (i = 0; i < width; i++) {
+            *p++ = i;
+            *p++ = green;
+            *p++ = j;
+            p++;
+        }
+    }
+}
+
