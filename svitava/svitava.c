248d774e5ee27b6b75446cd8112519511fa194cb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-07 09:36:16 +0100

    Rendering test image containing palette

--- svitava/svitava.c
+++ svitava/svitava.c
@@ -33,3 +33,20 @@ void render_test_rgb_image(unsigned int width, unsigned int height,
     }
 }
 
+void render_test_palette_image(unsigned int width, unsigned int height,
+                               const unsigned char *palette,
+                               unsigned char *pixels) {
+    unsigned int i, j;
+    unsigned char *p = pixels;
+
+    for (j = 0; j < height; j++) {
+        for (i = 0; i < width; i++) {
+            int color = i * 3;
+            *p++ = palette[color];
+            *p++ = palette[color + 1];
+            *p++ = palette[color + 2];
+            p++;
+        }
+    }
+}
+
