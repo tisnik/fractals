f90594ecc319f9473f9c2cf121f5c91ce8e95a3c
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-12 15:46:56 +0100

    Julia fractal renderer

--- svitava/svitava.c
+++ svitava/svitava.c
@@ -89,6 +89,50 @@ void render_mandelbrot(unsigned int width, unsigned int height,
     }
 }
 
+void render_julia(unsigned int width, unsigned int height,
+                  const unsigned char *palette, unsigned char *pixels,
+                  double cx, double cy) {
+    int x, y;
+    double zx0, zy0;
+    double xmin = -1.5, ymin = -1.5, xmax = 1.5, ymax = 1.5;
+    unsigned char *p = pixels;
+
+#ifdef DEBUG
+    printf("%f %f\n", cx, cy);
+#endif
+
+    zy0 = ymin;
+    for (y = 0; y < height; y++) {
+        zx0 = xmin;
+        for (x = 0; x < width; x++) {
+            double zx = zx0;
+            double zy = zy0;
+            unsigned int i = 0;
+            while (i < 150) {
+                double zx2 = zx * zx;
+                double zy2 = zy * zy;
+                if (zx2 + zy2 > 4.0) {
+                    break;
+                }
+                zy = 2.0 * zx * zy + cy;
+                zx = zx2 - zy2 + cx;
+                i++;
+            }
+            {
+                unsigned char *pal =
+                    (unsigned char *)palette + (unsigned char)(i * 3);
+
+                *p++ = *pal++;
+                *p++ = *pal++;
+                *p++ = *pal;
+                p++;
+            }
+            zx0 += (xmax - xmin) / width;
+        }
+        zy0 += (ymax - ymin) / height;
+    }
+}
+
 void render_circle_pattern(unsigned int width, unsigned int height,
                            const unsigned char *palette,
                            unsigned char *pixels) {
