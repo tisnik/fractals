00f94232d2c61d0b9565c25d7a56dc0ba3a8bc14
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-03 09:06:23 +0200

    Fixed imports

--- demo/mandelbrot_screen.py
+++ demo/mandelbrot_screen.py
@@ -14,16 +14,16 @@
 
 """Mandelbrot screen."""
 
-import pygame
 import sys
+
+import pygame
 from PIL import Image
-
-from demo.screen import Screen
-from demo.colors import Colors
-
-from demo.resources import Resources
 from pygame.surface import Surface
+
 from complex.mandelbrot import recalc_fractal
+from demo.colors import Colors
+from demo.resources import Resources
+from demo.screen import Screen
 
 
 class MandelbrotScreen(Screen):
