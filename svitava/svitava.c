4bf9ca79cbe3eb64b814eb4892c4e35020f08097
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-05 08:21:56 +0200

    Ability to write ASCII PPM images

--- svitava/svitava.c
+++ svitava/svitava.c
@@ -17,6 +17,9 @@ build as shared library: gcc -shared -Wl,-soname,testlib -o testlib.so -fPIC
 testlib.c build as executable:
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 
 void render_test_rgb_image(unsigned int width, unsigned int height,
                            unsigned char *pixels, unsigned char green) {
@@ -449,3 +452,51 @@ void render_circle_pattern(unsigned int width, unsigned int height,
     }
 }
 
+void ppm_write_ascii_to_stream(unsigned int width, unsigned int height, unsigned char *pixels, FILE *fout)
+{
+    int x, y;
+    unsigned char r, g, b;
+    unsigned char *p = pixels;
+
+    /* header */
+    fprintf(fout, "P3 %d %d 255\n", width, height);
+
+    /* pixel array */
+    for (y=height-1; y>=0; y--) {
+        for (x=0; x<width; x++) {
+            unsigned char r, g, b;
+            r = *p++;
+            g = *p++;
+            b = *p++;
+            p++;
+            fprintf(fout, "%d %d %d\n", r, g, b);
+        }
+    }
+}
+
+int ppm_write_ascii(unsigned int width, unsigned int height, unsigned char *pixels, const char *file_name)
+{
+    FILE *fout;
+
+    fout = fopen(file_name, "wb");
+    if (!fout)
+    {
+        return -1;
+    }
+
+    ppm_write_ascii_to_stream(width, height, pixels, fout);
+
+    if (fclose(fout) == EOF)
+    {
+        return -1;
+    }
+    return 0;
+}
+
+int main(void) {
+#define WIDTH 256
+#define HEIGHT 256
+    unsigned char *pixels = (unsigned char*) malloc(WIDTH*HEIGHT*4);
+    render_test_rgb_image(WIDTH, HEIGHT, pixels, 0);
+    ppm_write_ascii(WIDTH, HEIGHT, pixels, "test_rgb_1.ppm");
+}
