044f2acda34766b033fa8677cd97528d8672b599
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-22 08:39:33 +0200

    Barnsley J3 fractal renderer

---
+++ complex/barnsley_j3.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+"""Renderer of the classic Barnsley J3 fractal."""
+
+from PIL import Image
+
+# textura by mela byt ctvercova a jeji sirka i vyska by mela byt
+# mocninou cisla 2
+IMAGE_WIDTH = 512
+IMAGE_HEIGHT = 512
+
+
+def barnsley_j3(zx0, zy0, cx, cy, maxiter):
+    """Calculate number of iterations for given complex number to escape from set."""
+    c = complex(cx, cy)
+    z = complex(zx0, zy0)
+    for i in range(maxiter):
+        if abs(z) > 2:
+            return i
+        if z.real > 0:
+            zx = z.real*z.real - z.imag*z.imag - 1
+            zy = 2*z.real * z.imag
+        else:
+            zx = z.real*z.real - z.imag*z.imag - 1 + c.real*z.real
+            zy = 2*z.real * z.imag + c.imag*z.real
+        z = complex(zx, zy)
+
+    return 0
+
+
+def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, cx, cy, maxiter=1000):
+    """Recalculate the whole fractal and render the set into given image."""
+    width, height = image.size  # rozmery obrazku
+    stepx = (xmax - xmin) / width
+    stepy = (ymax - ymin) / height
+
+    y1 = ymin
+    for y in range(height):
+        x1 = xmin
+        for x in range(width):
+            i = barnsley_j3(x1, y1, cx, cy, maxiter)
+            i = 3 * i % 256
+            color = (palette[i][0], palette[i][1], palette[i][2])
+            image.putpixel((x, y), color)
+            x1 += stepx
+        y1 += stepy
+
+
+def main():
+    import palette_blues
+    import palette_gold
+    import palette_mandmap
+
+    image = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+    recalc_fractal(image, palette_blues.palette, -2.0, -2.0, 2.0, 2.0, 0.4, 1.5, 500)
+    image.save("barnsley_j3_1.png")
+
+    recalc_fractal(image, palette_blues.palette, -2.0, -2.0, 2.0, 2.0, -1.0, 0.2, 500)
+    image.save("barnsley_j3_2.png")
+
+    recalc_fractal(image, palette_gold.palette, -1.5, -1.5, 1.5, 1.5, -1.0, 1.0, 500)
+    image.save("barnsley_j3_3.png")
+
+
+if __name__ == "__main__":
+    main()
