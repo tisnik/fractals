f495d309f4343acd1d08048ee7b3a30b5a836e7b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-24 10:07:06 +0200

    Added comments + reformatted

--- attractors/3D/Wang-Sun.py
+++ attractors/3D/Wang-Sun.py
@@ -1,30 +1,42 @@
 """Výpočet a vykreslení Wan-Sunova podivného atraktoru."""
 
-# coding: utf-8
+# MIT License
+#
+# Copyright (c) 2020 Pavel Tišnovský
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 # # The Wang - Sun attractor
 # Please also see https://hipwallpaper.com/view/9W3CM8
-
-# In[1]:
 
 
 # import všech potřebných knihoven - Numpy a Matplotlibu
 import matplotlib.pyplot as plt
 import numpy as np
 
-# In[2]:
 
-
-# funkce pro výpočet dalšího bodu Wang-Sunova atraktoru
 def wang_sun(x, y, z, alfa, beta, gamma, delta, epsilon, zeta):
     """Výpočet dalšího bodu Wang-Sunova atraktoru."""
     x_dot = x * alfa + gamma * y * z
     y_dot = x * beta + y * delta - x * z
     z_dot = z * epsilon + zeta * x * y
     return x_dot, y_dot, z_dot
-
-
-# In[14]:
 
 
 # krok (změna času)
@@ -38,10 +50,6 @@ x = np.zeros((n,))
 y = np.zeros((n,))
 z = np.zeros((n,))
 
-
-# In[15]:
-
-
 # počáteční hodnoty
 x[0], y[0], z[0] = (1.05, 1.1, 1.5)
 
@@ -52,24 +60,27 @@ for i in range(n - 1):
     y[i + 1] = y[i] + y_dot * dt
     z[i + 1] = z[i] + z_dot * dt
 
-fig = plt.figure()
+# konstrukce 3D grafu
+fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(projection="3d")
+
+# změna velikosti komponent v grafu.
+plt.tight_layout()
 
 # vykreslení grafu
 ax.plot(x, y, z)
 
+# uložení grafu
+plt.savefig("wang_sun_1.png")
+
 # zobrazení grafu
-plt.tight_layout()
 plt.show()
 
-
-# In[16]:
-
-
+# grafy s více pohledy na atraktor
 ch_3d = np.stack((x, y, z))
 lim_xyz = [(np.min(ch_3d[ii]), np.max(ch_3d[ii])) for ii in range(3)]
 
-fig2 = plt.figure("3D Coordinates")
+fig2 = plt.figure("3D Coordinates", figsize=(8, 6))
 plt.subplot(2, 2, 1)
 plt.plot(y, x, linewidth=0.75)
 plt.grid()
@@ -99,7 +110,11 @@ ax.plot(x, y, z, linewidth=0.7)
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Z")
-plt.tight_layout()
 
 plt.tight_layout()
+
+# uložení grafu
+plt.savefig("wang_sun_2.png")
+
+# zobrazení grafu
 plt.show()
