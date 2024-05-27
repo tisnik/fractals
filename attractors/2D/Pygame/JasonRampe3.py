c99a914d0a76af34ac4cf34e5d7d01b0a20d4a2e
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-27 08:41:22 +0200

    Jason Rampe strange attractor #3 renderer

---
+++ attractors/2D/Pygame/JasonRampe3.py
@@ -0,0 +1,63 @@
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+
+from math import cos, sin
+from typing import Tuple
+
+from renderer import render_attractor
+from ui import event_loop, initialize
+
+# window settings
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+WINDOW_TITLE = "Jason Rampe dynamical system #3"
+
+
+def jason_rampe_3(
+    x: float, y: float, A: float, B: float, C: float, D: float
+) -> Tuple[float, float]:
+    """Calculate next point in the strange attractor."""
+    x_dot = sin(B * y) + C * cos(B * x)
+    y_dot = cos(A * x) + D * sin(A * y)
+    return x_dot, y_dot
+
+
+def main() -> None:
+    # initialize user interface based on Pygame
+    display, surface, clock = initialize(WINDOW_TITLE, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+    # redraw the whole dynamical system
+    render_attractor(
+        surface=surface,
+        x=0.1,
+        y=0.1,
+        scale=100.0,
+        x_offset=0,
+        y_offset=0,
+        max_points=1000000,
+        settle_down_points=10,
+        attractor_formula=jason_rampe_3,
+        contrast=1 / 60.0,
+        filename="jason_rampe_1.png",
+        A=2.0246,
+        B=-1.323,
+        C=-2.8151,
+        D=0.2277,
+    )
+
+    # and enter the event loop
+    event_loop(display, surface, clock)
+
+
+if __name__ == "__main__":
+    main()
