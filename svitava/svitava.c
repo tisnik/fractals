a148ab19c40f513bcb22c720c14425c3bec828a1
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-12 14:51:42 +0200

    Julia renderer for z^4+c

--- svitava/svitava.c
+++ svitava/svitava.c
@@ -277,6 +277,56 @@ void render_mandelbrot_4(unsigned int width, unsigned int height,
     }
 }
 
+void render_julia_4(unsigned int width, unsigned int height,
+                  const unsigned char *palette, unsigned char *pixels,
+                  double cx, double cy, int maxiter) {
+    int x, y;
+    double zx0, zy0;
+    double xmin = -1.5, ymin = -1.5, xmax = 1.5, ymax = 1.5;
+    unsigned char *p = pixels;
+
+#ifdef DEBUG
+    printf("%f %f\n", cx, cy);
+#endif
+
+    zy0 = ymin;
+    for (y = 0; y < height; y++) {
+        zx0 = xmin;
+        for (x = 0; x < width; x++) {
+            double zx = zx0;
+            double zy = zy0;
+            unsigned int i = 0;
+            while (i < maxiter) {
+                double zx2 = zx * zx;
+                double zy2 = zy * zy;
+                double zx4, zy4;
+                double zxn, zyn;
+                if (zx2 + zy2 > 4.0) {
+                    break;
+                }
+                zxn=zx2-zy2;
+                zyn=2.0*zx*zy;
+                zx4=zxn*zxn;
+                zy4=zyn*zyn;
+                zy=2.0*zxn*zyn+cy;
+                zx=zx4-zy4+cx;
+                i++;
+            }
+            {
+                unsigned char *pal =
+                    (unsigned char *)palette + (unsigned char)(i * 3);
+
+                *p++ = *pal++;
+                *p++ = *pal++;
+                *p++ = *pal;
+                p++;
+            }
+            zx0 += (xmax - xmin) / width;
+        }
+        zy0 += (ymax - ymin) / height;
+    }
+}
+
 void render_barnsley_m1(unsigned int width, unsigned int height,
                         const unsigned char *palette, unsigned char *pixels,
                         int maxiter) {
