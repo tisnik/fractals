76bb0543a9e469673efb250908cf8554401c2fd4
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-18 10:30:38 +0200

    Lambda(fn) fractal

---
+++ complex/lambda_fn.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+"""Renderer of the Lambda fractal based on selected function."""
+
+from PIL import Image
+import math
+import cmath
+
+# textura by mela byt ctvercova a jeji sirka i vyska by mela byt
+# mocninou cisla 2
+IMAGE_WIDTH = 512
+IMAGE_HEIGHT = 512
+
+
+def julia(zx0, zy0, cx, cy, maxiter):
+    """Calculate number of iterations for given complex numbers Z and C to escape from set."""
+    c = complex(cx, cy)
+    z = complex(zx0, zy0)
+    for i in range(maxiter):
+        if abs(z) > 64:
+            return i
+        z = c * cmath.sin(z)
+    return 0
+
+
+def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, cx, cy, maxiter=1000):
+    """Recalculate the whole fractal and render the set into given image."""
+    width, height = image.size  # rozmery obrazku
+    stepx = (xmax - xmin) / width
+    stepy = (ymax - ymin) / height
+
+    y1 = ymin
+    for y in range(height):
+        x1 = xmin
+        for x in range(width):
+            i = julia(x1, y1, cx, cy, maxiter)
+            i = 3 * i % 256
+            color = (palette[i][0], palette[i][1], palette[i][2])
+            image.putpixel((x, y), color)
+            x1 += stepx
+        y1 += stepy
+
+
+def main():
+    import palette_blues
+    import palette_gold
+    import palette_mandmap
+
+    image = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+    recalc_fractal(image, palette_blues.palette, -3.0, -3.0, 3.0, 3.0, 1.0, 0.4, 250)
+    image.save("lambda_fn_1.png")
+
+    #recalc_fractal(image, palette_mandmap.palette, -0.25, -0.7, 0.75, 0.3, 0.85, 0.6, 1000)
+    #image.save("lambda_2.png")
+
+
+if __name__ == "__main__":
+    main()
