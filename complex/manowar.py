a5218118b73619b9bc2938589573668f442feb27
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-22 10:13:35 +0200

    Manowar fractal: Mandelbrot varian

---
+++ complex/manowar.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+"""Renderer of the Manowar fractal."""
+
+from PIL import Image
+
+# textura by mela byt ctvercova a jeji sirka i vyska by mela byt
+# mocninou cisla 2
+IMAGE_WIDTH = 512
+IMAGE_HEIGHT = 512
+
+
+def julia(zx0, zy0, cx, cy, maxiter):
+    """Calculate number of iterations for given complex number to escape from set."""
+    c = complex(cx, cy)
+    z = c
+    z1 = c
+
+    for i in range(maxiter):
+        if abs(z) > 2:
+            return i
+        z2 = z * z + z1 + c
+        z1 = z
+        z = z2
+    return 0
+
+
+def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, maxiter=1000):
+    """Recalculate the whole fractal and render the set into given image."""
+    width, height = image.size  # rozmery obrazku
+    stepx = (xmax - xmin) / width
+    stepy = (ymax - ymin) / height
+
+    y1 = ymin
+    for y in range(height):
+        x1 = xmin
+        for x in range(width):
+            i = mandelbrot(x1, y1, maxiter)
+            i = 3 * i % 256
+            color = (palette[i][0], palette[i][1], palette[i][2])
+            image.putpixel((x, y), color)
+            x1 += stepx
+        y1 += stepy
+
+
+def main():
+    import palette_mandmap
+
+    image = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+    recalc_fractal(image, palette_mandmap.palette, -1.5, -1.0, 0.5, 1.0, 1000)
+    image.save("manowar.png")
+
+
+if __name__ == "__main__":
+    main()
