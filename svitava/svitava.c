cc8249e0343828eae76421d4aa600d5a67be216c
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-06-28 08:28:50 +0200

    Refactoring putpixel in render_barnsley_j3

--- svitava/svitava.c
+++ svitava/svitava.c
@@ -519,15 +519,7 @@ void render_barnsley_j3(unsigned int width, unsigned int height,
                 zy = zyn;
                 i++;
             }
-            {
-                unsigned char *pal =
-                    (unsigned char *)palette + (unsigned char)(i * 3);
-
-                *p++ = *pal++;
-                *p++ = *pal++;
-                *p++ = *pal;
-                p++;
-            }
+            putpixel(&p, palette, i);
             zx0 += (xmax - xmin) / width;
         }
         zy0 += (ymax - ymin) / height;
