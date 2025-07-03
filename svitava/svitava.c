ef2243448e097f682652e31e24db2becfc68f512
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-03 09:34:48 +0200

    Refactoring putpixel in render_magnet_mj

--- svitava/svitava.c
+++ svitava/svitava.c
@@ -755,15 +755,7 @@ void render_magnet_j2(unsigned int width, unsigned int height,
                 zy = 2.0 * zxn * zyn;
                 i++;
             }
-            {
-                unsigned char *pal =
-                    (unsigned char *)palette + (unsigned char)(i * 3);
-
-                *p++ = *pal++;
-                *p++ = *pal++;
-                *p++ = *pal;
-                p++;
-            }
+            putpixel(&p, palette, i);
             cx0 += (xmax - xmin) / width;
         }
         cy0 += (ymax - ymin) / height;
