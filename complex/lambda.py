2eb600767b00737194b6af491f1ace79a45fe2e5
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-23 13:51:44 +0200

    Added license

--- complex/lambda.py
+++ complex/lambda.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python
+#
+#  (C) Copyright 2025  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
 
 """Renderer of the classic Lambda fractal."""
 
