0f851be987f39b30f5916d479f623c4455e6de55
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-07-24 08:21:33 +0200

    Added license

--- complex/alpha_composite.py
+++ complex/alpha_composite.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+
+
 """Alpha channel-based composition of two fractals into one image."""
 
 import palette_blues
