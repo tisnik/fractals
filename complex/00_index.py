ed4efd2303a09b5d392b453f8912a909e83099e3
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-08-19 09:30:29 +0200

    Updated index file

--- complex/00_index.py
+++ complex/00_index.py
@@ -1,3 +1,32 @@
 # List of source files stored in this directory
 # ---------------------------------------------
 #
+# alpha_composite.py:
+# Alpha channel-based composition of two fractals into one image.
+#
+# barnsley_j1.py:
+# Renderer of the classic Barnsley J1 fractal.
+#
+# barnsley_j1_interactive.py:
+# Interactive renderer of the classic Barnsley J1 fractal based on Pygame.
+#
+# barnsley_j2.py:
+# Renderer of the classic Barnsley J2 fractal.
+#
+# barnsley_j2_interactive.py:
+# Interactive renderer of the classic Barnsley J2 fractal based on Pygame.
+#
+# barnsley_j3.py:
+# Renderer of the classic Barnsley J3 fractal.
+#
+# barnsley_j3_interactive.py:
+# Interactive renderer of the classic Barnsley J3 fractal based on Pygame.
+#
+# barnsley_m1.py:
+# Renderer of the classic Barnsley M1 fractal.
+#
+# barnsley_m2.py:
+# Renderer of the classic Barnsley M2 fractal.
+#
+# barnsley_m3.py:
+# Renderer of the classic Barnsley M3 fractal.
