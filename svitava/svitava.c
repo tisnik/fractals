3a65c3a5b4a7265f59f13dd0b4327ff0e4d3c630
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-03 09:33:08 +0200

    Refactor Barnsley J1 renderer to use image_t structure as parameter

--- svitava/svitava.c
+++ svitava/svitava.c
@@ -489,18 +489,17 @@ void render_barnsley_m1(const image_t *image, const unsigned char *palette,
  * Colors are assigned from the palette based on the number of iterations before
  * escape or reaching `maxiter`.
  */
-void render_barnsley_j1(unsigned int width, unsigned int height,
-                        const unsigned char *palette, unsigned char *pixels,
+void render_barnsley_j1(const image_t *image, const unsigned char *palette,
                         double cx, double cy, int maxiter) {
     int x, y;
     double zx0, zy0;
     double xmin = -2.0, ymin = -2.0, xmax = 2.0, ymax = 2.0;
-    unsigned char *p = pixels;
+    unsigned char *p = image->pixels;
 
     zy0 = ymin;
-    for (y = 0; y < height; y++) {
+    for (y = 0; y < image->height; y++) {
         zx0 = xmin;
-        for (x = 0; x < width; x++) {
+        for (x = 0; x < image->width; x++) {
             double zx = zx0;
             double zy = zy0;
             unsigned int i = 0;
@@ -523,9 +522,9 @@ void render_barnsley_j1(unsigned int width, unsigned int height,
                 i++;
             }
             putpixel(&p, palette, i);
-            zx0 += (xmax - xmin) / width;
+            zx0 += (xmax - xmin) / image->width;
         }
-        zy0 += (ymax - ymin) / height;
+        zy0 += (ymax - ymin) / image->height;
     }
 }
 
@@ -1822,6 +1821,8 @@ int render_test_images(void) {
     render_barnsley_m1(&image, palette, 1000);
     bmp_write(WIDTH, HEIGHT, pixels, "barnsley_m1.bmp");
 
+    render_barnsley_j1(&image, palette, 0.484375, -1.21875, 1000);
+    bmp_write(WIDTH, HEIGHT, pixels, "barnsley_j1.bmp");
     /*
     render_test_rgb_image(WIDTH, HEIGHT, pixels, 0);
     ppm_write_ascii(WIDTH, HEIGHT, pixels, "test_rgb_1.ppm");
