63edfed29cfee249ab911cd533c19791ca1a3a7d
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-05 08:45:12 +0200

    Bailout as constants, added docstrings etc.

--- complex/barnsley_j3.py
+++ complex/barnsley_j3.py
@@ -14,20 +14,25 @@
 #      Pavel Tisnovsky
 #
 
+from time import time
+
 from PIL import Image
 
-# textura by mela byt ctvercova a jeji sirka i vyska by mela byt
-# mocninou cisla 2
+# image size specified in pixels
+# the size of the image should be square, and its height and width
+# should be an integer power of 2
 IMAGE_WIDTH = 512
 IMAGE_HEIGHT = 512
 
+# bailout value
+BAILOUT = 2
 
 def barnsley_j3(zx0, zy0, cx, cy, maxiter):
     """Calculate number of iterations for given complex number to escape from set."""
     c = complex(cx, cy)
     z = complex(zx0, zy0)
     for i in range(maxiter):
-        if abs(z) > 2:
+        if abs(z) > BAILOUT:
             return i
         if z.real > 0:
             zx = z.real*z.real - z.imag*z.imag - 1
@@ -59,21 +64,39 @@ def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, cx, cy, maxiter=1000)
 
 
 def main():
+    """Function called after the script initialization."""
     import palette_blues
     import palette_gold
     import palette_mandmap
 
+    # construct new image
     image = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
 
+    print("Calculation started")
+    t1 = time()
     recalc_fractal(image, palette_blues.palette, -2.0, -2.0, 2.0, 2.0, 0.4, 1.5, 500)
     image.save("barnsley_j3_1.png")
+    t2 = time()
+    difftime = t2 - t1
+    print(f"Calculation finished in {difftime:4.1f} seconds")
 
+    print("Calculation started")
+    t1 = time()
     recalc_fractal(image, palette_blues.palette, -2.0, -2.0, 2.0, 2.0, -1.0, 0.2, 500)
     image.save("barnsley_j3_2.png")
+    t2 = time()
+    difftime = t2 - t1
+    print(f"Calculation finished in {difftime:4.1f} seconds")
 
+    print("Calculation started")
+    t1 = time()
     recalc_fractal(image, palette_gold.palette, -1.5, -1.5, 1.5, 1.5, -1.0, 1.0, 500)
     image.save("barnsley_j3_3.png")
+    t2 = time()
+    difftime = t2 - t1
+    print(f"Calculation finished in {difftime:4.1f} seconds")
 
 
 if __name__ == "__main__":
+    # call the main function
     main()
