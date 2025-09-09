449e590e6cf697881885a35b423171ed2dab4f18
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-09 08:13:27 +0200

    Pixmap creation + freeing

--- svitava/ui.c
+++ svitava/ui.c
@@ -1,13 +1,17 @@
 #include <SDL2/SDL.h>
 
 /* graphics options */
-#define WIDTH 640
-#define HEIGHT 480
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 300
+#define IMAGE_WIDTH 256
+#define IMAGE_HEIGHT 256
 #define TITLE "Svitava"
 
 typedef struct GraphicsState {
     SDL_Window *window;
     SDL_Surface *screen_surface;
+    SDL_Surface *mandelbrot_pixmap;
+    SDL_Surface *julia_pixmap;
 } GraphicsState;
 
 static void init_sdl(GraphicsState *graphicsState, const char *title,
@@ -45,9 +49,36 @@ static void init_sdl(GraphicsState *graphicsState, const char *title,
 }
 
 static void finalize(GraphicsState *graphicsState) {
+    if (graphicsState->mandelbrot_pixmap != NULL) {
+        printf("Freeing pixmap (%p)\n", (void*)graphicsState->mandelbrot_pixmap);
+        free(graphicsState->mandelbrot_pixmap);
+    }
+    if (graphicsState->julia_pixmap != NULL) {
+        printf("Freeing pixmap (%p)\n", (void*)graphicsState->julia_pixmap);
+        free(graphicsState->julia_pixmap);
+    }
+
     SDL_FreeSurface(graphicsState->screen_surface);
     SDL_DestroyWindow(graphicsState->window);
     SDL_Quit();
+}
+
+static SDL_Surface *create_pixmap(const int width, const int height) {
+    SDL_Surface *pixmap;
+    pixmap = SDL_CreateRGBSurface(SDL_SWSURFACE, width, height, 32, 0x00ff0000,
+                                  0x0000ff00, 0x000000ff, 0x00000000);
+    if (!pixmap) {
+        puts("Can not create pixmap");
+        exit(1);
+    } else {
+        printf("Off screen pixmap created (%p)\n", (void*)pixmap);
+    }
+    return pixmap;
+}
+
+static void create_pixmaps(GraphicsState *graphicsState, const int width, const int height) {
+    graphicsState->mandelbrot_pixmap=create_pixmap(width, height);
+    graphicsState->julia_pixmap=create_pixmap(width, height);
 }
 
 void redraw(GraphicsState *graphicsState) {
@@ -86,7 +117,8 @@ static void main_event_loop(GraphicsState *graphicsState) {
 int main(int argc, char **argv) {
     GraphicsState graphicsState;
 
-    init_sdl(&graphicsState, TITLE, WIDTH, HEIGHT);
+    init_sdl(&graphicsState, TITLE, SCREEN_WIDTH, SCREEN_HEIGHT);
+    create_pixmaps(&graphicsState, IMAGE_WIDTH, IMAGE_HEIGHT);
 
     main_event_loop(&graphicsState);
 
