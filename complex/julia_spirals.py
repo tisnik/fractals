3ea6be5faf4a58f1d0f8ef9fd795b332df0d2bcb
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-09 08:12:50 +0200

    Added license text

--- complex/julia_spirals.py
+++ complex/julia_spirals.py
@@ -2,6 +2,19 @@
 
 """Julia fractal - some spiral variants."""
 
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+
 from PIL import Image
 
 IMAGE_WIDTH = 512
