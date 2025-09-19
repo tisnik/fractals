e5f70200ce462e73cc5893e51859bd8a88745081
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-09-19 09:21:01 +0200

    New Mandelbrot variant fn||fn||iter

---
+++ complex/mandel_fn_fn_iter.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+"""Renderer of Mandelbrot fractal variant fn||fn."""
+
+#
+#  (C) Copyright 2024  Pavel Tisnovsky
+#
+#  All rights reserved. This program and the accompanying materials
+#  are made available under the terms of the Eclipse Public License v1.0
+#  which accompanies this distribution, and is available at
+#  http://www.eclipse.org/legal/epl-v10.html
+#
+#  Contributors:
+#      Pavel Tisnovsky
+#
+
+
+import cmath
+
+from time import time
+
+from PIL import Image
+
+# image size specified in pixels
+# the size of the image should be square, and its height and width
+# should be an integer power of 2
+IMAGE_WIDTH = 512
+IMAGE_HEIGHT = 512
+
+# bailout value
+BAILOUT = 64
+
+def mandelbrot_fn_fn(cx, cy, maxiter):
+    """Calculate number of iterations for given complex number to escape from set."""
+    c = complex(cx, cy)
+    z = 0
+    for i in range(maxiter):
+        if abs(z) > BAILOUT:
+            return i
+        if i % 2 == 0:
+            z = cmath.sin(z) + c
+        else:
+            z = z*z + c
+    return 0
+
+
+def recalc_fractal(image, palette, xmin, ymin, xmax, ymax, maxiter=1000):
+    """Recalculate the whole fractal and render the set into given image."""
+    width, height = image.size  # image size in pixels
+    stepx = (xmax - xmin) / width
+    stepy = (ymax - ymin) / height
+
+    y1 = ymin
+    for y in range(height):
+        x1 = xmin
+        for x in range(width):
+            i = mandelbrot_fn_fn(x1, y1, maxiter)
+            i = 3 * i % 256
+            color = (palette[i][0], palette[i][1], palette[i][2])
+            image.putpixel((x, y), color)
+            x1 += stepx
+        y1 += stepy
+        print(".", end="")
+
+
+def main():
+    import palette_mandmap
+
+    # construct new image
+    image = Image.new("RGB", (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+    print("Calculation started")
+    t1 = time()
+    recalc_fractal(image, palette_mandmap.palette, -2.0, -1.5, 1.0, 1.5, 1000)
+    image.save("mandelbrot_fn_fn.png")
+    t2 = time()
+    difftime = t2 - t1
+    print(f"Calculation finished in {difftime:4.1f} seconds")
+
+
+if __name__ == "__main__":
+    main()
