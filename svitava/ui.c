490c2eb4107bd851b1de28efe63d343f454fb105
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-10-17 12:20:21 +0200

    Background rendering

--- svitava/ui.c
+++ svitava/ui.c
@@ -1,11 +1,15 @@
 #include <SDL2/SDL.h>
 
 /* graphics options */
-#define SCREEN_WIDTH 600
-#define SCREEN_HEIGHT 300
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 #define IMAGE_WIDTH 256
 #define IMAGE_HEIGHT 256
 #define TITLE "Svitava"
+
+#define ABS(a) ((a)<0 ? -(a) : (a) )
+#define MAX(a,b) ((a)>(b) ? (a) : (b))
+#define MIN(a,b) ((a)<(b) ? (a) : (b))
 
 typedef struct GraphicsState {
     SDL_Window *window;
@@ -85,6 +89,69 @@ void redraw(GraphicsState *graphicsState) {
     SDL_UpdateWindowSurface(graphicsState->window);
 }
 
+void gfx_putpixel(SDL_Surface *surface, int x, int y, unsigned char r, unsigned char g, unsigned char b)
+{
+    if (x>=0 && x< surface->w && y>=0 && y < surface->h)
+    {
+        if (surface->format->BitsPerPixel == 24)
+        {
+            Uint8 *pixel = (Uint8 *)surface->pixels;
+            pixel += x*3;
+            pixel += y*surface->pitch;
+            *pixel++ = b;
+            *pixel++ = g;
+            *pixel   = r;
+        }
+        if (surface->format->BitsPerPixel == 32)
+        {
+            Uint8 *pixel = (Uint8 *)surface->pixels;
+            pixel += x*4;
+            pixel += y*surface->pitch;
+            *pixel++ = b;
+            *pixel++ = g;
+            *pixel   = r;
+        }
+    }
+}
+
+void gfx_hline(SDL_Surface *surface, int x1, int x2, int y, unsigned char r, unsigned char g, unsigned char b)
+{
+    int x;
+    int fromX = MIN(x1, x2);
+    int toX = MAX(x1, x2);
+    for (x = fromX; x <= toX; x++)
+    {
+        gfx_putpixel(surface, x, y, r, g, b);
+    }
+}
+
+void gfx_vline(SDL_Surface *surface, int x,  int y1, int y2, unsigned char r, unsigned char g, unsigned char b)
+{
+    int y;
+    int fromY = MIN(y1, y2);
+    int toY = MAX(y1, y2);
+    for (y = fromY; y <= toY; y++)
+    {
+        gfx_putpixel(surface, x, y, r, g, b);
+    }
+}
+void draw_grid(GraphicsState *graphicsState) {
+    SDL_Surface *surface = graphicsState->screen_surface;
+    int width = surface->w;
+    int height = surface->h;
+    int x, y;
+    SDL_FillRect(surface, NULL, 0xffffffff);
+
+    for (x=0; x<width; x+=20)
+    {
+        gfx_vline(surface, x, 0, height-1, 191, 191, 255);
+    }
+    for (y=0; y<height; y+=20)
+    {
+        gfx_hline(surface, 0, width-1, y, 191, 191, 255);
+    }
+}
+
 static void main_event_loop(GraphicsState *graphicsState) {
     SDL_Event event;
     int done = 0;
@@ -120,6 +187,7 @@ int main(int argc, char **argv) {
     init_sdl(&graphicsState, TITLE, SCREEN_WIDTH, SCREEN_HEIGHT);
     create_pixmaps(&graphicsState, IMAGE_WIDTH, IMAGE_HEIGHT);
 
+    draw_grid(&graphicsState);
     main_event_loop(&graphicsState);
 
     finalize(&graphicsState);
