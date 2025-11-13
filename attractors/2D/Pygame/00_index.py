9600c339fa6b805ad2d13ecbb6f1ff0a5a789853
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-11-13 10:25:34 +0100

    Generated index file

---
+++ attractors/2D/Pygame/00_index.py
@@ -0,0 +1,51 @@
+# List of source files stored in this directory
+# ---------------------------------------------
+#
+# Bedhead.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Bedhead.
+#
+# De_jong.py:
+# Výpočet a vykreslení De Jongova podivného atraktoru.
+#
+# Fractal_dream.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Fractal Dream.
+#
+# Gumowski-Mira.py:
+# Výpočet a vykreslení podivného atraktoru vytvořeného I.Gumowskim a L.Mirou.
+#
+# Hopalong.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Hopalong.
+#
+# JasonRampe1.py:
+# Výpočet a vykreslení podivného atraktoru vytvořeného Jasonem Rampem 1.
+#
+# JasonRampe2.py:
+# Výpočet a vykreslení podivného atraktoru vytvořeného Jasonem Rampem 2.
+#
+# JasonRampe3.py:
+# Výpočet a vykreslení podivného atraktoru vytvořeného Jasonem Rampem 3.
+#
+# Pickover.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného 2D Pickover.
+#
+# Quadruptwo.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Quadruptwo.
+#
+# Svensson.py:
+# Výpočet a vykreslení podivného atraktoru navrženého Johny Svenssonem.
+#
+# Threeply.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Threeply.
+#
+# bitmap.py:
+# Bitmap operations.
+#
+# colors.py:
+# Enumeration containing colors.
+#
+# renderer.py:
+# Pygame-based renderer.
+#
+# ui.py:
+# Basic UI for Pygame-based renderer.
+#
