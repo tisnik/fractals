2798edaf91353e26398dc09306beca263a7f79e0
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-11-13 10:25:23 +0100

    Generated index file

---
+++ attractors/2D/00_index.py
@@ -0,0 +1,48 @@
+# List of source files stored in this directory
+# ---------------------------------------------
+#
+# Bedhead.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Bedhead.
+#
+# De_jong.py:
+# Výpočet a vykreslení De Jongova podivného atraktoru.
+#
+# Dynamic.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Dynamic.
+#
+# Fractal_dream.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Fractal Dream.
+#
+# Gumowski-Mira.py:
+# Výpočet a vykreslení podivného atraktoru vytvořeného I.Gumowskim a L.Mirou.
+#
+# Hopalong.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Hopalong.
+#
+# Icon.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Symmetric Icon.
+#
+# JasonRampe1.py:
+# Výpočet a vykreslení podivného atraktoru vytvořeného Jasonem Rampem 1.
+#
+# JasonRampe2.py:
+# Výpočet a vykreslení podivného atraktoru vytvořeného Jasonem Rampem 2.
+#
+# JasonRampe3.py:
+# Výpočet a vykreslení podivného atraktoru vytvořeného Jasonem Rampem 3.
+#
+# Kam_Torus.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Kam torus.
+#
+# Pickover.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného 2D Pickover.
+#
+# Quadruptwo.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Quadruptwo.
+#
+# Svensson.py:
+# Výpočet a vykreslení podivného atraktoru navrženého Johny Svenssonem.
+#
+# Threeply.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Threeply.
+#
