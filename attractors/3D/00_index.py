ea824eee6cd4c532e463ec83c997ba973d1fea41
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-11-13 10:25:39 +0100

    Generated index file

---
+++ attractors/3D/00_index.py
@@ -0,0 +1,48 @@
+# List of source files stored in this directory
+# ---------------------------------------------
+#
+# Lorenz-mod-2.py:
+# Výpočet a vykreslení upraveného Lorenzova podivného atraktoru.
+#
+# Lorenz.py:
+# Výpočet a vykreslení Lorenzova podivného atraktoru.
+#
+# Lorenz_butterfly_effect.py:
+# Výpočet a vykreslení Lorenzova podivného atraktoru s efektem motýlích křídel.
+#
+# Lorenz_x_y_z.py:
+# Výpočet a vykreslení hodnot x, y, z Lorenzova podivného atraktoru.
+#
+# Pickover.py:
+# Výpočet a vykreslení Pickoverova podivného atraktoru v 3D.
+#
+# Rossler.py:
+# Výpočet a vykreslení Rosslerova podivného atraktoru v 3D.
+#
+# Wang-Sun.py:
+# Výpočet a vykreslení Wang-Sunova podivného atraktoru.
+#
+# lorenz_2d_dynamic.py:
+# Vykreslení Lorenzova atraktoru s různými počátečními souřadnicemi x0 a y0.
+#
+# lorenz_2d_dynamic_multiplot.py:
+# Vykreslení Lorenzova atraktoru s různými parametry s a r.
+#
+# lorenz_change_params.py:
+# Vykreslení Lorenzova atraktoru s různými parametry s a r.
+#
+# lorenz_change_params_anim.py:
+# Vykreslení Lorenzova atraktoru s různými počátečními souřadnicemi x0 a y0.
+#
+# lorenz_change_params_anim_2.py:
+# Vykreslení Lorenzova atraktoru s různými počátečními souřadnicemi x0 a y0.
+#
+# lorenz_change_params_multiplot.py:
+# Vykreslení Lorenzova atraktoru s různými parametry s a r.
+#
+# lorenz_map.py:
+# Map of Lorenz attractor.
+#
+# rossler_map.py:
+# Map of Rossler attractor.
+#
