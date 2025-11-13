3dd0e719224be54dd6b612fc710730b1861e0db7
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-11-13 10:25:29 +0100

    Generated index file

---
+++ attractors/2D/PIL/00_index.py
@@ -0,0 +1,12 @@
+# List of source files stored in this directory
+# ---------------------------------------------
+#
+# Bedhead.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Bedhead.
+#
+# Quadruptwo.py:
+# Výpočet a vykreslení podivného atraktoru nazvaného Quadruptwo.
+#
+# bitmap.py:
+# Bitmap operations.
+#
