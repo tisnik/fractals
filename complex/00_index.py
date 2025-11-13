aa70d3ac6e5ac78d8fe75ec892dbc43f8998cb5b
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-11-13 10:25:51 +0100

    Generated index file

--- complex/00_index.py
+++ complex/00_index.py
@@ -30,3 +30,139 @@
 #
 # barnsley_m3.py:
 # Renderer of the classic Barnsley M3 fractal.
+#
+# blend_add.py:
+# Blending based on averaging two images without using alpha channel.
+#
+# blending.py:
+# Blending of two fractal images.
+#
+# colors.py:
+# Named colors used everywhere on demo screens.
+#
+# julia.py:
+# Renderer of the classic Julia fractal.
+#
+# julia_fn.py:
+# Renderer of the Lambda Julia fractal based on selected function.
+#
+# julia_fn_fn.py:
+# Renderer of Julia fractal variant fn||fn.
+#
+# julia_fn_fn_interactive.py:
+# Interactive renderer of the Julia fractal fn||fn based on Pygame.
+#
+# julia_fn_fn_iter.py:
+# Renderer of Julia fractal variant fn||fn||iter.
+#
+# julia_interactive.py:
+# Interactive renderer of the classic Julia fractal based on Pygame.
+#
+# julia_spirals.py:
+# Julia fractal - some spiral variants.
+#
+# julia_z^2-z_interactive.py:
+# Interactive renderer of the Julia fractal z^z-z based on Pygame.
+#
+# lambda.py:
+# Renderer of the classic Lambda fractal.
+#
+# lambda_fn.py:
+# Renderer of the Lambda fractal based on selected function.
+#
+# lambda_interactive.py:
+# Interactive Lambda fractal renderer.
+#
+# magnet_j1.py:
+# Renderer of the classic Magnet J1 fractal.
+#
+# magnet_m1.py:
+# Renderer of the classic Magnet M1 fractal.
+#
+# magnet_m2.py:
+# Renderer of the classic Magnet M2 fractal.
+#
+# mandel_fn.py:
+# Renderer of the Lambda fractal based on selected function.
+#
+# mandel_fn_fn.py:
+# Renderer of Mandelbrot fractal variant fn||fn.
+#
+# mandel_fn_fn_iter.py:
+# Renderer of Mandelbrot fractal variant fn||fn.
+#
+# mandel_fn_no_cmath.py:
+# Renderer of the Lambda fractal based on selected function.
+#
+# mandel_lambda.py:
+# Renderer of the classic Mandelbrot lambda fractal.
+#
+# mandelbrot.py:
+# Renderer of the classic Mandelbrot fractal.
+#
+# mandelbrot_spirals.py:
+# Renderer of the classic Mandelbrot fractal: spiral forms.
+#
+# mandelbrot_z^2+z.py:
+# Renderer of Mandelbrot fractal variant z^2+z.
+#
+# mandelbrot_z^2-z.py:
+# Renderer of Mandelbrot fractal variant z^2-z.
+#
+# mandelbrot_z^3-z.py:
+# Renderer of Mandelbrot fractal variant z^3-z.
+#
+# mandelbrot_z^3.py:
+# Renderer of Mandelbrot fractal variant z^3.
+#
+# mandelbrot_z^4.py:
+# Renderer of Mandelbrot fractal variant z^4.
+#
+# manowar.py:
+# Renderer of the Manowar fractal.
+#
+# manowarj.py:
+# Renderer of the Manowar fractal.
+#
+# newton.py:
+# Renderer of the classic Newton fractal.
+#
+# palette_blues.py:
+# Color palette taken from Fractint.
+#
+# palette_gold.py:
+# Color palette taken from Fractint.
+#
+# palette_greens.py:
+# Color palette taken from Fractint.
+#
+# palette_ice.py:
+# Color palette taken from Fractint.
+#
+# palette_juteblue.py:
+# Color palette taken from Fractint.
+#
+# palette_jutemap.py:
+# Color palette taken from Fractint.
+#
+# palette_jutes.py:
+# Color palette taken from Fractint.
+#
+# palette_mandmap.py:
+# Color palette taken from Fractint.
+#
+# palette_phong.py:
+# Color palette taken from Fractint.
+#
+# palette_rose.py:
+# Color palette taken from Fractint.
+#
+# phoenix_interactive.py:
+# Phoenix interactive fractal renderer.
+#
+# phoenix_j.py:
+# Renderer of the Phoenix fractal, Julia version.
+#
+# phoenix_m.py:
+# Renderer of the Phoenix fractal, Mandelbrot version.
+#
